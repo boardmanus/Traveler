3d364ab5876a0666157861e55c04cd002aff8fcb
Craig Boardman <craig_boardman@trimble.com>
2022-05-14 17:48:44 +1200

    Make traveller conform to scrups ts
    Fix a number of typing issues, add extra definitions, added some
    helper functions to access data

--- index.d.ts
+++ index.d.ts
@@ -1,59 +1,76 @@
+interface CreepMemory {
+  _trav?: TravelData;
+}
+
+interface RoomMemory {
+  avoid?: boolean;
+}
+
 interface PathfinderReturn {
-    path: RoomPosition[];
-    ops: number;
-    cost: number;
-    incomplete: boolean;
+  path: RoomPosition[];
+  ops: number;
+  cost: number;
+  incomplete: boolean;
 }
 
 interface TravelToReturnData {
-    nextPos?: RoomPosition;
-    pathfinderReturn?: PathfinderReturn;
-    state?: TravelState;
-    path?: string;
+  nextPos?: RoomPosition;
+  nextDir?: DirectionConstant;
+  pathfinderReturn?: PathfinderReturn;
+  state?: TravelState;
+  path?: string;
 }
 
 interface TravelToOptions {
-    ignoreRoads?: boolean;
-    ignoreCreeps?: boolean;
-    ignoreStructures?: boolean;
-    preferHighway?: boolean;
-    highwayBias?: number;
-    allowHostile?: boolean;
-    allowSK?: boolean;
-    range?: number;
-    obstacles?: {pos: RoomPosition}[];
-    roomCallback?: (roomName: string, matrix: CostMatrix) => CostMatrix | boolean;
-    routeCallback?: (roomName: string) => number;
-    returnData?: TravelToReturnData;
-    restrictDistance?: number;
-    useFindRoute?: boolean;
-    maxOps?: number;
-    movingTarget?: boolean;
-    freshMatrix?: boolean;
-    offRoad?: boolean;
-    stuckValue?: number;
-    maxRooms?: number;
-    repath?: number;
-    route?: {[roomName: string]: boolean};
-    ensurePath?: boolean;
+  justPath?: boolean;
+  ignoreRoads?: boolean;
+  ignoreCreeps?: boolean;
+  ignoreStructures?: boolean;
+  preferHighway?: boolean;
+  highwayBias?: number;
+  allowHostile?: boolean;
+  allowSK?: boolean;
+  range?: number;
+  obstacles?: { pos: RoomPosition }[];
+  roomCallback?: (roomName: string, matrix: CostMatrix) => CostMatrix | boolean;
+  routeCallback?: (roomName: string) => number;
+  returnData?: TravelToReturnData;
+  restrictDistance?: number;
+  useFindRoute?: boolean;
+  maxOps?: number;
+  movingTarget?: boolean;
+  freshMatrix?: boolean;
+  offRoad?: boolean;
+  stuckValue?: number;
+  maxRooms?: number;
+  repath?: number;
+  route?: { [roomName: string]: boolean };
+  ensurePath?: boolean;
 }
 
 interface TravelData {
-    state: any[];
-    path: string;
+  state?: {
+    prevX: number;
+    prevY: number;
+    stuck: number;
+    cpu: number;
+    destX: number;
+    destY: number;
+    roomName: string;
+  };
+  path?: string;
 }
 
 interface TravelState {
-    stuckCount: number;
-    lastCoord: Coord;
-    destination: RoomPosition;
-    cpu: number;
+  stuckCount: number;
+  lastCoord: Coord;
+  destination: RoomPosition;
+  cpu: number;
 }
 
 interface Creep {
-    travelTo(destination: HasPos|RoomPosition, ops?: TravelToOptions): number;
+  travelTo(destination: HasPos | RoomPosition, ops?: TravelToOptions): number;
 }
 
-type Coord = {x: number, y: number};
-type HasPos = {pos: RoomPosition}
-
+type Coord = { x: number; y: number };
+type HasPos = { pos: RoomPosition };
